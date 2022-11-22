db65ace65d694521fcf1b1fc26562ad94284603e
Thales <bthales312@gmail.com>
2022-11-22 13:22:57 -0300

    exericio 17 python Brasil

---
+++ exercícios/ex17.py
@@ -0,0 +1,13 @@
+# 17 Faça um Programa que peça um número correspondente a um determinado ano e em seguida informe se este ano é ou não bissexto.
+
+for _ in range(5):
+    from datetime import date
+    ano = int(input('qual ano? '))
+    if ano == 0:
+        ano = date.today().year
+    # ocorrendo a cada quatro anos. Exceto anos múltiplos de 100 que não são múltiplos de 400
+    if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
+        print(f'o ano {ano} é  bissexto')
+    else:
+        print(f'o ano {ano} não é bissexto')
+        
