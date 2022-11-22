6ce5d1989a571a73f01753008d3ff3a50013207b
Thales <90719778+thalesBre@users.noreply.github.com>
2022-11-22 13:28:25 -0300

    Update ex17.py

--- exercícios/ex17.py
+++ exercícios/ex17.py
@@ -1,3 +1,4 @@
+# ESTRUTURA DE DECISAO PYTHON BRASIL
 # 17 Faça um Programa que peça um número correspondente a um determinado ano e em seguida informe se este ano é ou não bissexto.
 
 for _ in range(5):
